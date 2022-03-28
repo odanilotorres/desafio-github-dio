7ddbc143669232e2548d215d4fd4fa25715ea8f4
odanilotorres <torres.danilo5@gmail.com>
2022-03-27 21:07:33 -0300

    Adicionando informações sobre lista e tuplas no python

---
+++ Introdução a Programação com Python/aula005.py
@@ -0,0 +1,36 @@
+# # criando uma lista
+#
+lista = [1, 15, 5, 2]
+# print(sum(lista)) #somar os itens da lista
+# print(max(lista)) #maior valor da lista
+# print(min(lista)) #menor valor da lista
+
+lista_animais = ['Carrocho', 'Gato', 'Elefante']
+# print(lista_animais[1])
+#
+# for x in lista_animais:
+#     print(x)
+#
+#
+# # Saber se um elemento está na lista
+# if 'Gato' in lista_animais:
+#     print('O gato está')
+# else:
+#     print('O não gato está')
+#     lista_animais.append('gato') #adicionar um valor na lista
+#
+# lista_animais.pop() #retira a última posição da lista, ou posso colocar qual posição que quero retirar
+# print(lista_animais)
+#
+# lista_animais.remove('Gato') #remover o valor pelo nome
+# print(lista_animais)
+#
+# lista.sort() #ordenar
+# lista_animais.sort() #ordenar
+# print(lista)
+# print(lista_animais)
+# lista_animais.reverse() #inverter a lista
+# print(lista_animais)
+
+tupla = (1, 10 , 12, 14)
+print(tupla)
