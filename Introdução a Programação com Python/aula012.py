eb2c0d27bc451ccd94e00511ecdadcc0c6f4b05a
odanilotorres <torres.danilo5@gmail.com>
2022-03-31 22:16:42 -0300

    Finalizando introdução ao python

--- Introdução a Programação com Python/aula012.py
+++ Introdução a Programação com Python/aula012.py
@@ -2,5 +2,8 @@
 
 import requests
 
-response = requests.get('https://viacep.com.br/ws/01001000/json/')
-print(response.status_code)
+response = requests.get('https://viacep.com.br/ws/18046525/json/')
+# print(response.status_code)
+print(response.text)
+dados_cep = response.json()
+print('Logradouro: ' + dados_cep['logradouro'])
