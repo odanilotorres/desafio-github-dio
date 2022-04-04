2daf5b22e9e91ba281d7423ffe172ffd6ca729b0
odanilotorres <torres.danilo5@gmail.com>
2022-04-04 20:24:44 -0300

    Novos recursos python

--- seguranca-com-python/ping_multiplo.py
+++ seguranca-com-python/ping_multiplo.py
@@ -9,6 +9,7 @@ with open('hosts') as file:
     print('<>' * 30)
     print('-' * 60)
     for ip in dump:
+        print(('-' * 15) + ' Escaneando IP: {} '.format(ip) + ('-' * 15))
         os.system('ping -c 4 {}'.format(ip))
         print('-' * 60)
         print('<>' * 30)
